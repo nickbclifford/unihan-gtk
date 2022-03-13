5150c244166052eefb8e6aeccf25708e788e0c17
Nick Clifford <nick@nickclifford.me>
2022-03-12 19:05:27 -0600

    import DB

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
+mod read;
+
+use gtk::gio::Cancellable;
+use gtk::glib::clone;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Label};
+use gtk::{
+    Application, ApplicationWindow, Button, FileChooserAction, FileChooserNative, FileFilter,
+    ResponseType,
+};
 
 fn build_ui(app: &Application) {
-    let label = Label::builder()
-        .label("Hello, world!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
+    let filter = FileFilter::new();
+    filter.add_mime_type("application/zip");
+
+    let open_file_button = Button::builder().label("Open database file").build();
+
+    let dialog = FileChooserNative::builder()
+        .modal(true)
+        .title("Select Unihan database file")
+        .action(FileChooserAction::Open)
+        .filter(&filter)
+        .accept_label("Open")
+        .cancel_label("Cancel")
         .build();
+
+    dialog.connect_response(|this, rt| {
+        if rt == ResponseType::Accept {
+            // Files are Send but the read streams aren't, so move the file
+            let file = this.file().unwrap();
+
+            std::thread::spawn(move || {
+                read::init_db(file.read(None::<&Cancellable>).unwrap().into_read()).unwrap();
+            });
+        }
+    });
+
+    open_file_button.connect_clicked(clone!(@strong dialog => move |_| {
+        dialog.show();
+    }));
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("unihan-gtk")
-        .child(&label)
+        .child(&open_file_button)
         .build();
+
+    dialog.set_transient_for(Some(&window));
 
     window.present();
 }
---
+++ src/read.rs
@@ -0,0 +1,51 @@
+use once_cell::sync::Lazy;
+use rusqlite::{params, Connection};
+use std::{
+    error::Error,
+    io::{BufRead, BufReader, Read, Seek},
+    sync::Mutex,
+};
+use zip::ZipArchive;
+
+pub static DB: Lazy<Mutex<Connection>> =
+    Lazy::new(|| Mutex::new(Connection::open_in_memory().unwrap()));
+
+pub fn init_db(zipfile: impl Read + Seek) -> Result<(), Box<dyn Error>> {
+    let mut zip = ZipArchive::new(zipfile)?;
+
+    let mut conn = DB.lock()?;
+
+    conn.execute(
+        "CREATE TABLE field (
+            character INTEGER NOT NULL,
+            name      TEXT NOT NULL,
+            value     TEXT NOT NULL,
+            PRIMARY KEY (character, name)
+        )",
+        [],
+    )?;
+
+    for i in 0..zip.len() {
+        let file = zip.by_index(i)?;
+        let tx = conn.transaction()?;
+        for line in BufReader::new(file).lines() {
+            let line = line?;
+            if !line.starts_with("U+") {
+                continue;
+            }
+
+            let cols: Vec<_> = line.split('\t').collect();
+
+            // trim off "U+", parse from hex
+            let char_value = u32::from_str_radix(&cols[0][2..], 16)?;
+
+            tx.execute(
+                "INSERT INTO field VALUES (?1, ?2, ?3)",
+                params![char_value, cols[1], cols[2]],
+            )?;
+        }
+        tx.commit()?;
+    }
+
+    Ok(())
+}
